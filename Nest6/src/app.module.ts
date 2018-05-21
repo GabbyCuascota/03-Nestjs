bf70063af4ab0ed857b21c7541295ffcc3bc024f
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-05-21 09:56:46 -0500

    actualizar nest6

--- Nest6/src/app.module.ts
+++ Nest6/src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {UsuarioController} from "./usuario.controller";
+import {UsuarioService} from "./usuario.service";
+import {ParametrosController} from "./parametros.controller";
+import {LogMiddleware} from './log.middleware';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'web2018agr2.mysql.database.azure.com',
+          port: 3306,
+          username: 'profesor@web2018agr2',
+          password: 'Javascript1',
+          database: 'web',
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+          ssl:true
+      }),
+
+  ],
+  controllers: [AppController,
+      UsuarioController,
+      ParametrosController,],
+  providers: [AppService,
+      UsuarioService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    nombreAplicacion = 'EPN';
+
+    configure(consumer: MiddlewareConsumer)
+        : void {
+        consumer
+            .apply(LogMiddleware)
+            .with(this.nombreAplicacion, 1989)
+            .forRoutes(
+                UsuarioController,
+                AppController,
+                ParametrosController
+            )
+        //.apply(OtroMiddleware)
+        //.forRoutes(Otras rutas);
+    }
+}
--- Nest6/src/usuario.controller.ts
+++ Nest6/src/usuario.controller.ts
@@ -1,20 +1,30 @@
 
-import {Controller, Get, HttpCode, Req, Res} from "@nestjs/common";
+import {Controller, Get, HttpCode, Post, Req, Res} from "@nestjs/common";
 import Status = jest.Status;
+import {UsuarioService} from "./usuario.service";
 
 // decorator
 @Controller('Usuario')
 export class UsuarioController {
     usuario = {
-        nombre: 'Gabriela',
-        apellido: 'Cuascota',
-        edad: 26
+        nombre: 'Adrian',
+        apellido: 'Eguez',
+        edad: 28
     };
+
+    usuarios = [];
+
+    constructor(private _usuarioService: UsuarioService) {
+
+    }
 
     @HttpCode(202)
     @Get('mostrar')
-    mostrarUsuario() {
-        return this.usuario;
+    mostrarUsuario(
+        @Res() response
+    ) {
+        const usuarios = this._usuarioService.mostrarUsuarios();
+        return response.send(usuarios);
     }
 
     @Get('mostrarExpress')
@@ -23,7 +33,27 @@ export class UsuarioController {
         @Res() response
     ) {
         return response
-            .status(500)
-            .send(this.usuario);
+            .status(200)
+            .send(this.usuarios);
     }
+
+    @Post('crearUsuario')
+    crearUsuario(
+        @Req() request,
+        @Res() response
+    ) {
+        const nuevoUsuario = {
+            nombre: request.query.nombre,
+            apellido: request.query.apellido,
+            edad: request.query.edad
+        };
+
+        const usuarioCreado = this._usuarioService.crearUsuario(nuevoUsuario);
+
+        return response
+            .status(201)
+            .send(usuarioCreado);
+    }
+
+
 }
