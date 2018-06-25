f39def61b91a552ef91fad266016d3fd69e6d1fc
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-06-25 10:56:27 -0500

    bindings

--- 05-angular/App/src/app/app.component.ts
+++ 05-angular/App/src/app/app.component.ts
@@ -14,4 +14,19 @@ export class AppComponent {
   };
   arregloNumeros = [1,2,3,4,5];
   mostrarTitulo = true;
+  imagenUrl = "https://minutodedios.fm/wp-content/uploads/2017/05/perro-gato.jpg";
+  imagenWidth = '300';
+  html = `
+  <h1>HOLA</h1>
+  <p>Hola mundo</p>
+  <img src="${this.imagenUrl}" alt="">
+`;
+
+  aumentarWidth(evento){
+    console.log('evento',evento);
+    this.imagenWidth += 10;
+  }
+  restarWidth(){
+    this.imagenWidth -= 10;
+  }
 }
